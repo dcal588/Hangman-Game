23c39104524a45c2e0bc744488ebebfbd1f69d4f
dcal588 <calderon.david@gmail.com>
2017-07-11 15:36:57 -0700

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,16 @@
     $(document).ready(function() {
-      var words=['wordone','wordtwo','wordthree','wordfour','wordfive']
-      var hints=['hintone','hinttwo','hintthree','hintfour','hintfive']
-      var randomNumber = Math.floor(Math.random() * words.length)
+      var words=['wordone','wordtwo','wordthree','wordfour','wordfive'];
+      var hints=['hintone','hinttwo','hintthree','hintfour','hintfive'];
+      var randomNumber = Math.floor(Math.random() * words.length);
       var randomWord = words[randomNumber];
       var randomHint = hints[randomNumber];
       var randomLetters = randomWord.split('');
-      var guessesMade = 0
+      var guessesMade = 0;
       var guessesAvailable = randomWord.length + 5;
       var blankSpaces = blankSpacesStart(randomWord);
 
       function guessAreaKeyPress() {
-        var guessArea = document.getElementById("guessArea");
+        var guessAreaKeyPress = document.getElementById("guessArea");
         var s = guessArea.value;
         var guesses = document.getElementById("guesses");
         guesses.innerText = s;

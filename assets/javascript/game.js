896271f30ada57f1afab2da6fcb33db7c481cfdf
dcal588 <calderon.david@gmail.com>
2017-11-28 12:56:36 -0800

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,50 @@
-   
-      var words=['wordone','wordtwo','wordthree','wordfour','wordfive'];
-      var hints=['hintone','hinttwo','hintthree','hintfour','hintfive'];
-      var randomNumber = Math.floor(Math.random() * words.length);
-      var randomWord = words[randomNumber];
-      var randomHint = hints[randomNumber];
-      var randomLetters = randomWord.split('');
-      var guessesMade = 0;
-      var guessesAvailable = randomWord.length + 5;
-      var blankSpaces = blankSpacesStart(randomWord);
-      var letterGuesses = [];
+var words=['wordone','wordtwo','wordthree','wordfour','wordfive'];
+var hints=['hintone','hinttwo','hintthree','hintfour','hintfive'];
+var randomNumber = Math.floor(Math.random() * words.length);
+var randomWord = words[randomNumber];
+var randomHint = hints[randomNumber];
+var randomLetters = randomWord.split('');
+var guessesMade = 0;
+var guessesAvailable = randomWord.length + 5;
+var blankSpaces = blankSpacesStart(randomWord);
+var letterGuesses = [];
 
-      document.onkeypress = function (e) {
-        e = e || window.event;
-        letterGuesses.push(e.key);
-        document.getElementById("guesses").innerHTML = letterGuesses;
-        checker(e);
-      };
+document.onkeypress = function (e) {
+  e = e || window.event;
+  letterGuesses.push(e.key);
+  document.getElementById("guesses").innerHTML = letterGuesses;
+  checker(e);
+};
 
-      function blankSpacesStart (randomWord){
-        var spaces = '';
-        for (var i = 0; i < randomWord.length; i++){
-          spaces = spaces + ' _';
-        }
-        var progressDiv = document.getElementById("progress");
-          progressDiv.innerHTML = spaces;
-      }
-      var hintDiv = document.getElementById("hint");
-        hintDiv.innerHTML = randomHint;
+function blankSpacesStart (randomWord) {
+  var spaces = '';
+  for (var i = 0; i < randomWord.length; i++) {
+    spaces = spaces + ' _';
+  }
+  var progressDiv = document.getElementById("progress");
+  progressDiv.innerHTML = spaces;
+}
+
+var hintDiv = document.getElementById("hint");
+hintDiv.innerHTML = randomHint;
       
-      if (guessesMade === guessesAvailable) {
-        messageDiv.innerHTML = "Game Over";
-      }
-      var messageDiv = document.getElementById("message");
+if (guessesMade === guessesAvailable) {
+  messageDiv.innerHTML = "Game Over";
+}
 
-      function checker (e){
-        var i = randomLetters.indexOf(e.key);
-        console.log(e.key);
-        console.log(randomLetters);
-        console.log(i);
-        if (i !== -1) {
-          randomLetters.splice(i,1);
-          console.log(i);
-        }
-        var progressDiv = document.getElementById("progress");
-          progressDiv.innerHTML = spaces;
-      }
+var messageDiv = document.getElementById("message");
 
+function checker (e){
+  var i = randomLetters.indexOf(e.key);
+  console.log("ekey " + e.key);
+  console.log("variable i :" + i);
+  if (i !== -1) {
+    spaces.splice(i,1,);
+    console.log("variable 1 number 2: " + i);
+    progressDiv.innerHTML = randomLetters;
+    console.log("random letters " + randomLetters);
+  }
+  var progressDiv = document.getElementById("progress");
+  progress.innerHTML = spaces;
+}
  
